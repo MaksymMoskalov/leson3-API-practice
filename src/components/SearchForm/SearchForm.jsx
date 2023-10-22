469fa86b75fba2eb0bc45ae9bb59f6e53896cbe8
Tosha177777 <slavkoanton4@gmail.com>
2023-10-22 21:10:10 +0300

    countryList

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -1,14 +1,24 @@
 import { FiSearch } from 'react-icons/fi';
 import { BtnSearch, Select, SearchFormStyled } from './SearchForm.styled';
+import { useState } from 'react';
 
-export const SearchForm = () => {
+export const SearchForm = ({ setQuery }) => {
+  const [option, setOption] = useState('');
+  const handleSubmit = e => {
+    e.preventDefault();
+    setQuery(option);
+    setOption('');
+  };
+  const onChange = e => {
+    setOption(e.target.value);
+  };
   return (
-    <SearchFormStyled>
+    <SearchFormStyled onSubmit={handleSubmit}>
       <BtnSearch type="submit">
         <FiSearch size="16px" />
       </BtnSearch>
-      <Select aria-label="select" name="region" required>
-        <option selected disabled defaultValue="">
+      <Select aria-label="select" name="region" onChange={onChange} required>
+        <option disabled defaultValue="">
           Select a region and press enter
         </option>
         <option value="africa">Africa</option>
--- src/pages/CountrySearch.jsx
+++ src/pages/CountrySearch.jsx
@@ -6,12 +6,28 @@ import {
   Loader,
   CountryList,
 } from 'components';
+import { useEffect, useState } from 'react';
+import { fetchByRegion } from 'service/country-service';
 
 export const CountrySearch = () => {
+  const [query, setQuery] = useState('');
+  console.log('query: ', query);
+  useEffect(() => {
+    if (!query) {
+      return;
+    }
+    const searchInfo = async () => {
+      try {
+        const infoData = await fetchByRegion(query);
+        console.log('infoData: ', infoData);
+      } catch (error) {}
+    };
+    searchInfo();
+  }, [query]);
   return (
     <Section>
       <Container>
-        <h2>CountrySearch</h2>
+        <SearchForm setQuery={setQuery} />
       </Container>
     </Section>
   );
